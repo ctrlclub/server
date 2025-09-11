18f0cedf85cee567e19cf1ce01df6cc257bd4ba8
Mqlvin <pricklydotcactus@gmail.com>
2025-09-11 13:18:43 +0100

    add get challenge by id route + challenge loader

--- src/main/kotlin/club/ctrl/server/server/routes/Challenges.kt
+++ src/main/kotlin/club/ctrl/server/server/routes/Challenges.kt
@@ -2,6 +2,12 @@ package club.ctrl.server.server.routes
 
 import club.ctrl.server.challenges.ChallengeManager
 import club.ctrl.server.database.getChallengeMeta
+import club.ctrl.server.database.getChallengeSubmissions
+import club.ctrl.server.database.getChallengeUnlocked
+import club.ctrl.server.database.getLastViewed
+import club.ctrl.server.database.getWorkingAt
+import club.ctrl.server.database.hasViewed
+import club.ctrl.server.entity.respondError
 import club.ctrl.server.entity.respondSuccess
 import club.ctrl.server.server.UserIdKey
 import com.mongodb.client.MongoDatabase
@@ -11,6 +17,9 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class ChallengeListing(val challengeId: Int, val numSubchallenges: Int, val completedSubchallenges: Int, val unlocked: Boolean, val name: String)
+
+@Serializable
+data class SubchallengeContent(val subchallengeId: Int, val content: String, val completed: Boolean, val answer: String?)
 
 
 fun Route.challengesRoute(db: MongoDatabase) {
@@ -30,5 +39,68 @@ fun Route.challengesRoute(db: MongoDatabase) {
         }
 
         call.respondSuccess(challengeListingBuilder)
+    }
+
+    get("{id}") {
+        val userId = call.attributes[UserIdKey]
+
+        val idParameter = call.parameters["id"]
+        if(idParameter == null) {
+            call.respondError("No challenge ID provided")
+            return@get
+        }
+
+        val urlId = idParameter.toIntOrNull(10)
+        if(urlId == null) {
+            call.respondError("The challenge ID provided was not a number")
+            return@get
+        }
+
+        val id = urlId - 1
+        val challengeObj = ChallengeManager.challenges.getOrNull(id)
+        if(challengeObj == null) {
+            call.respondError("No challenge was found with the provided ID")
+            return@get
+        }
+
+        val unlocked = getChallengeUnlocked(id, db);
+        if(!unlocked) {
+            call.respondError("No challenge was found with the provided ID")
+            return@get
+        }
+
+        val subchallengeContents = mutableListOf<SubchallengeContent>()
+
+        val workingAt = getWorkingAt(userId, id, db) // working at subchallenge id
+        if(workingAt != null) { // if they still have subchallenges to complete
+            val viewed = hasViewed(userId, id, workingAt, db)
+            if(!viewed) {
+                // first we get the subchallenge to init anything it needs to
+                ChallengeManager.challenges[id].subchallenges[workingAt].onFirstOpen(userId)
+            }
+
+            // add the contents for the working at subchallenge
+            subchallengeContents.add(SubchallengeContent(
+                workingAt, // subchallenge id
+                ChallengeManager.challenges[id].subchallenges[workingAt].loadMarkdown(userId), // md content
+                false, // completed? is false cuz they're "workingAt" it. ha. ha. ok not funny
+                null // no completed answer ofc
+            ))
+
+
+        }
+
+        // then here we collate everything into the obj to send to the frontend
+        val submissions = getChallengeSubmissions(userId, id, db)
+        for(sub in submissions.reversed()) {
+            subchallengeContents.add(SubchallengeContent(
+                sub.subchallengeId,
+                ChallengeManager.challenges[id].subchallenges[sub.subchallengeId].loadMarkdown(userId),
+                true,
+                sub.answer
+            ))
+        }
+
+        call.respondSuccess(subchallengeContents.reversed())
     }
 }
