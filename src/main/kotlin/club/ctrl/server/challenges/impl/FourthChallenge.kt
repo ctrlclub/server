e7bab31ed8f4d500aea2a90eb49235e1a2277a2a
Mqlvin <pricklydotcactus@gmail.com>
2025-10-13 22:12:43 +0100

    add incomplete challenge4 to be modified

--- src/main/kotlin/club/ctrl/server/challenges/impl/FourthChallenge.kt
+++ src/main/kotlin/club/ctrl/server/challenges/impl/FourthChallenge.kt
@@ -3,13 +3,9 @@ package club.ctrl.server.challenges.impl
 import club.ctrl.server.challenges.Challenge
 import club.ctrl.server.challenges.ChallengeCollection
 import club.ctrl.server.challenges.Subchallenge
+import club.ctrl.server.database.findSerializable
 import club.ctrl.server.server.routes.SubmissionFeedback
 import com.mongodb.client.model.Filters
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.json.Json
-import org.bson.Document
-import java.util.Random
-import javax.xml.crypto.Data
 
 
 object FourthChallenge : Challenge {
@@ -20,7 +16,7 @@ object FourthChallenge : Challenge {
 }
 
 object FourthChallengeP0 : Subchallenge {
-    override val parent: Challenge = ThirdChallenge
+    override val parent: Challenge = FourthChallenge
     override val subchallengeIdx: Int = 0
 
     override fun onFirstOpen(userId: String, db: ChallengeCollection) {
@@ -32,7 +28,7 @@ object FourthChallengeP0 : Subchallenge {
 }
 
 object FourthChallengeP1 : Subchallenge {
-    override val parent: Challenge = ThirdChallenge
+    override val parent: Challenge = FourthChallenge
     override val subchallengeIdx: Int = 1
 
     override fun onFirstOpen(userId: String, db: ChallengeCollection) = Unit // subchallenge 0 inits all data
